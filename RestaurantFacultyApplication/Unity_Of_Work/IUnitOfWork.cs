ea36be9b331d84fb392c56dbd2bf7d2b66772e44
Nebojsa <soneneso93@gmail.com>
2016-12-03 16:21:16 +0100

    Added UnitOfWork pattern...

---
+++ RestaurantFacultyApplication/Unity_Of_Work/IUnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestaurantFacultyApplication.Repositories;
+namespace RestaurantFacultyApplication.Unity_Of_Work
+{
+    interface IUnitOfWork : IDisposable
+    {
+        ICustomerRepository Customers { get; }
+        IFriendRepository Friends { get; }
+        IFriendRatingRepository FriendsRating { get; }
+        IInvitationRepository Invitations { get; }
+        IMealRepository Meals { get; }
+        IReservationRepository Reservations { get; }
+        IResevatedTableRepository ResevatedTables { get; }
+        IRestaurantRepository Restaurants { get; }
+        ITableRepository Tables { get; }
+        IUserRepository Users { get; }
+        int Complete();
+    }
+}
---
+++ RestaurantFacultyApplication/Unity_Of_Work/UnitOfWork.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RestaurantFacultyApplication.Repositories;
+using RestaurantFacultyApplication.Persistence;
+namespace RestaurantFacultyApplication.Unity_Of_Work
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly RestaurantModelContext _context;
+        public UnitOfWork(RestaurantModelContext context) {
+            _context = context;
+            Customers = new CustomerRepository(_context);
+            Friends = new FriendRepository(_context);
+            FriendsRating = new FriendRatingRepository(_context);
+            Invitations = new InvitationRepository(_context);
+            Meals = new MealRepository(_context);
+            Reservations = new ReservationRepository(_context);
+            ResevatedTables = new ResevatedTableRepository(_context);
+            Restaurants = new RestaurantRepository(_context);
+            Tables = new TableRepository(_context);
+            Users = new UserRepository(_context);
+        }
+        public ICustomerRepository Customers { get; private set; }
+        public IFriendRepository Friends { get; private set; }
+        public IFriendRatingRepository FriendsRating { get; private set; }
+        public IInvitationRepository Invitations { get; private set; }
+        public IMealRepository Meals { get; private set; }
+        public IReservationRepository Reservations { get; private set; }
+        public IResevatedTableRepository ResevatedTables { get; private set; }
+        public IRestaurantRepository Restaurants { get; private set; }
+        public ITableRepository Tables { get; private set; }
+        public IUserRepository Users { get; private set; }
+        public int Complete()
+        {
+            return _context.SaveChanges();
+        }
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
